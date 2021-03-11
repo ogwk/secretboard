0e906df580360ea368236639e89a3a44ae894d13
ogwk <kanakkuma.0528@gmail.com>
2021-03-11 21:50:07 +0900

    雛形作成

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+const express = require('express');
+const app = express();
+
+
+//routing
+app.get('/posts',);
+app.get('/posts/detail',);
+app.post('/posts/detail',);
+app.post('/posts?delete=1',);
+app.get('/logout',);
+app.post('/favicon.ico',);
+app.get('/signup',);
+app.post('/signup',);
